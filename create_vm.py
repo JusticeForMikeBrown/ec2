50dea4c31e60212fc4648c78c0079880cde53a63
Douglas Duckworth <quackmaster@protonmail.com>
2018-10-02 14:00:31 -0400

    now writing dns for all vms to file

--- create_vm.py
+++ create_vm.py
@@ -5,6 +5,7 @@ __author__ = 'quackmaster@protonmail.com'
 import argparse
 import boto3
 from botocore.exceptions import ClientError
+from datetime import datetime
 
 parser = argparse.ArgumentParser()
 parser.add_argument('number', help='number of ec2 instances to create')
@@ -20,13 +21,16 @@ ec2 = boto3.resource('ec2')
 # set client var
 client = boto3.client('ec2')
 
+# ssh public key
+key = 'id_duckd_donut'
+
 
 # if absent create security group that enables ssh from world
 def enable_ssh():
 
     try:
         check_ssh = [client.describe_security_groups(GroupNames=['ssh'])]
-        print(check_ssh)
+        # print(check_ssh)
 
     except ClientError as e:
         if e.response['Error']['Code'] == 'InvalidGroup.NotFound':
@@ -47,11 +51,35 @@ def create_vm():
       MinCount=int(1),
       MaxCount=int(args.number),
       InstanceType=args.type,
-      SecurityGroups=['ssh']
+      SecurityGroups=['ssh'],
+      KeyName=key
     )
 
     instances
+
+    vm_dns = []
+
+    # get public dns of hosts once up
+    # then store dns names in a file
+    for vm in instances:
+        vm.wait_until_running()
+        vm.load()
+        print(vm.public_dns_name)
+        vm_dns.append(vm.public_dns_name)
+
     print("Created " + args.number + " VMs of type " + args.type)
+
+    now = datetime.now()
+
+    # write created vms to a file
+    with open("new_ec2_vms_%s" % now.strftime('%Y-%m-%d-%M'), "w") as f:
+        for item in vm_dns:
+            f.write("%s\n" % item)
 
 
 create_vm()
+
+
+# response = client.describe_instances(Filters=[{'Name': 'instance-state-name',
+#                                               'Values': ['pending']}],
+#                                     InstanceIds=[])
